d73925f4d55f361c130cb59942c711590e0a9320
Korèn <kpapazian@edenschool.fr>
2023-02-03 15:29:44 +0100

    js

--- menu/js/app.js
+++ menu/js/app.js
@@ -1,12 +1,15 @@
 let img_x = document.getElementById("img_x");
 let img_o = document.getElementById("img_o");
+let turn = document.getElementById("turn");
 
 img_x.addEventListener("click", () => {
     img_x.id = "img_x_on";
     img_o.id = "img_o";
+    // turn.style.backgroundImage = "url('../../img/icon-x-grey.svg')";
 });
 
 img_o.addEventListener("click", () => {
     img_o.id = "img_o_on";
     img_x.id = "img_x";
+    // turn.style.backgroundImage = "url('../../img/icon-o-grey.svg')";
 });
