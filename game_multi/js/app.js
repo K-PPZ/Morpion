37c71b100fcf818f5db91da9d58291a35fce36ba
Korèn <kpapazian@edenschool.fr>
2023-02-07 16:17:51 +0100

    solo multi js

---
+++ game_multi/js/app.js
@@ -0,0 +1,35 @@
+if (document.location.href == "http://127.0.0.1:5173/game_multi/game_multi.html?text=x") {
+    let cases = document.getElementsByClassName("cases_morpion");
+    let cpu = Math.floor(Math.random() * (8 - 0)) + 0;
+    let player_img = document.getElementById("player_img");
+    player_img.src = "../../img/icon-x-grey.svg";
+
+    for (let i = 0; i < cases.length; i++) {
+        cases[i].classList.add("hover_x");
+        let round = true;
+    
+        cases[i].addEventListener("click", () => {
+            if (round) {
+                cases[i].classList.add("case_x");
+            }
+        });
+    }
+}
+
+if (document.location.href == "http://127.0.0.1:5173/game_multi/game_multi.html?text=o") {
+    let cases = document.getElementsByClassName("cases_morpion");
+    let cpu = Math.floor(Math.random() * (8 - 0)) + 0;
+    let player_img = document.getElementById("player_img");
+    player_img.src = "../../img/icon-o-grey.svg";
+
+    for (let i = 0; i < cases.length; i++) {
+        cases[i].classList.add("hover_o");
+        let round = true;
+    
+        cases[i].addEventListener("click", () => {
+            if (round) {
+                cases[i].classList.add("case_o");
+            }
+        });
+    }
+}
--- menu/js/app.js
+++ menu/js/app.js
@@ -1,15 +1,19 @@
 let img_x = document.getElementById("img_x");
 let img_o = document.getElementById("img_o");
-let turn = document.getElementById("turn");
+
+let liens_game_solo = document.getElementById("btn_yellow");
+let liens_game_multi = document.getElementById("btn_blue");
 
 img_x.addEventListener("click", () => {
     img_x.id = "img_x_on";
     img_o.id = "img_o";
-    turn.style.backgroundImage = "url('../../img/icon-x-grey.svg')";
+    liens_game_solo.href = "../game_solo/game_solo.html?text=x";
+    liens_game_multi.href = "../game_multi/game_multi.html?text=x";
 });
 
 img_o.addEventListener("click", () => {
     img_o.id = "img_o_on";
     img_x.id = "img_x";
-    turn.style.backgroundImage = "url('../../img/icon-o-grey.svg')";
+    liens_game_solo.href = "../game_solo/game_solo.html?text=o";
+    liens_game_multi.href = "../game_multi/game_multi.html?text=o";
 });
