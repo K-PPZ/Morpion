7b23741d04b907e43b208b8a8aa24cee21c8859f
Korèn <kpapazian@edenschool.fr>
2023-02-22 15:07:11 +0100

    js

--- game_solo/js/app.js
+++ game_solo/js/app.js
@@ -1,18 +1,16 @@
 let url_x = new URL("http://127.0.0.1:5173/game_solo/game_solo.html?text=x");
 let url_o = new URL("http://127.0.0.1:5173/game_solo/game_solo.html?text=o");
 
-// let cases = document.getElementsByClassName("cases_morpion");
-// for (let i = 0; i < cases.length; i++) {
-//     cases[i].addEventListener("click", () => {
-//         console.log(i);
-//         console.log("cpu", Math.floor(Math.random() * (cases.length - 0)) + 0);
-//     });
-// }
 
 let restart = document.getElementById("restart");
+let player_img = document.getElementById("player_img");
+
+
 
 if (document.location.href == url_x) {
     let cases = document.getElementsByClassName("cases_morpion");
+
+    let turn = true;
 
     restart.addEventListener("click", () => {
         for (let i = 0; i < cases.length; i++) {
@@ -21,42 +19,121 @@ if (document.location.href == url_x) {
         }
     });
 
-    function round_player (cases) {
-        // let cases = document.getElementsByClassName("cases_morpion");
-        let player_img = document.getElementById("player_img");
+    function round_player (cases, turn) {
         player_img.src = "../../img/icon-x-grey.svg";
-    
+
         for (let i = 0; i < cases.length; i++) {
             cases[i].classList.add("hover_x");
+
             cases[i].addEventListener("click", () => {
                 cases[i].classList.add("case_x");
-                round_cpu(cases);
-                // cases[i].classList.remove("cases_morpion");
+                console.log("round_player ");
+
+                turn = false;
+                round(turn);
             });
+
         }
     }
-    round_player(cases);
 
-    function round_cpu (cases) {
-        // let cases = document.getElementsByClassName("cases_morpion");
-        let cpu = Math.floor(Math.random() * (cases.length - 0)) + 0;
-        let player_img = document.getElementById("player_img");
+    function round_cpu (cases, turn) {
         player_img.src = "../../img/icon-o-grey.svg";
+        let cpu = Math.floor(Math.random() * (cases.length - 0)) + 0;
 
-        if (cases[cpu].style.backgroundImage == "url('../../img/icon-x.svg')" || cases[cpu].style.backgroundImage == "url('../../img/icon-o.svg')") {
-            clearTimeout(interv_cpu);
-            round_cpu(cases);
-        } else {
-            let interv_cpu = setTimeout(() => {
-                cases[cpu].classList.add("case_o");
-                // cases[cpu].classList.remove("cases_morpion");
-                clearTimeout(interv_cpu);
-                round_player(cases);
-            }, 2000);
+        console.log("round_cpu ");
+
+        turn = true;
+        round(turn);
+    }
+
+    
+    
+    let turn_player = round_player(cases, turn);
+    let turn_cpu = round_cpu(cases, turn);
+
+    function round (turn) {
+        if (turn) {
+           turn_player;
+        }
+
+        if (turn == false) {
+            turn_cpu;
         }
     }
-    
+    round(turn);
+
+    // if (turn) {
+    //     turn = false;
+    //     round(turn);
+    // } else {
+    //     turn = true;
+    //     round(turn);
+    // }
 }
+
+
+
+
+
+
+
+
+
+// if (document.location.href == url_x) {
+//     let cases = document.getElementsByClassName("cases_morpion");
+
+//     restart.addEventListener("click", () => {
+//         for (let i = 0; i < cases.length; i++) {
+//             cases[i].classList.remove("case_x");
+//             cases[i].classList.remove("case_o");
+//         }
+//     });
+
+//     function round_player (cases) {
+//         let player_img = document.getElementById("player_img");
+//         player_img.src = "../../img/icon-x-grey.svg";
+    
+//         for (let i = 0; i < cases.length; i++) {
+//             cases[i].classList.add("hover_x");
+//             cases[i].addEventListener("click", () => {
+//                 cases[i].classList.add("case_x");
+//                 round_cpu(cases);
+//                 // cases[i].classList.remove("cases_morpion");
+//             });
+//         }
+//     }
+
+//     function round_cpu (cases) {
+//         let cpu = Math.floor(Math.random() * (cases.length - 0)) + 0;
+//         let player_img = document.getElementById("player_img");
+//         player_img.src = "../../img/icon-o-grey.svg";
+
+//         if (cases[cpu].style.backgroundImage == "url('../../img/icon-x.svg')" || cases[cpu].style.backgroundImage == "url('../../img/icon-o.svg')") {
+//             clearTimeout(interv_cpu);
+//             round_cpu(cases);
+//         } else {
+//             let interv_cpu = setTimeout(() => {
+//                 cases[cpu].classList.add("case_o");
+//                 // cases[cpu].classList.remove("cases_morpion");
+//                 clearTimeout(interv_cpu);
+//                 round_player(cases);
+//             }, 2000);
+//         }
+//     }
+
+//     function round (cases) {
+        
+//     }
+//     round(cases);
+    
+// }
+
+
+
+
+
+
+
 
 // if (document.location.href == url_o) {
 //     let cases = document.getElementsByClassName("cases_morpion");
